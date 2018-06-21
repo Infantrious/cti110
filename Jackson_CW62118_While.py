36f7e9b442d74abfc52b051fd275c019209d8aa4
Infantrious <40346665+Infantrious@users.noreply.github.com>
2018-06-21 13:14:47 -0400

    Add files via upload

---
+++ Jackson_CW62118_While.py
@@ -0,0 +1,18 @@
+# CW62118_While
+# Steven Jackson
+# 6/21/2018
+
+# While loop demo
+# Create a variable to hold a string literal
+
+choice = 'y'
+
+while choice == 'y':
+    #all your code goes here
+    print('x')
+    choice = input("Do u want to run the program again?" +
+                   " y or n ")
+
+
+
+
